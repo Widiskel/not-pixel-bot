8c05b75238bd584373d6043298ae37c309e90864
widiskel <waputra16@gmail.com>
2024-09-27 11:08:12 +0700

    feat (config) enable or disable upgrade

--- config/config_tmp.js
+++ config/config_tmp.js
@@ -2,4 +2,5 @@ export class Config {
   static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
   static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
   static RUNMODE = 2; // 1 FOR 1 BY 1 RUN & 2 FOR MASS RUN
+  static USEAUTOUPGRADE = true; //USE AUTO UPGRADE OR NO
 }
